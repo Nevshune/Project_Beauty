176ec4d05f94f0df223259d8f8a450345529fbe5
NamJiwan <129821189+NamJiwan@users.noreply.github.com>
2024-04-24 16:25:47 +0900

    페이지네이션

--- login_test_2/contest.php
+++ login_test_2/contest.php
@@ -3,7 +3,7 @@ include "./inc/common.php";
 include "./inc/dbconfig.php";
 
 // 페이지당 표시할 항목 수
-$itemsPerPage = 2;
+$itemsPerPage = 5;
 
 // 현재 페이지 번호 가져오기
 if (isset($_GET['page']) && is_numeric($_GET['page'])) {
