d2602550dfca35e0f892e17f3337dcb76abcd349
Nevshune <129821188+Nevshune@users.noreply.github.com>
2024-05-10 16:43:57 +0900

    240510_2nd_commit
    웹폰트 notosans 추가
    map.php 분리
    디테일 조정

---
+++ section_map.php
@@ -0,0 +1,23 @@
+<!-- 지도 -->
+
+<div class="w-full h-screen flex flex-col justify-center items-center bg-[#FAFAFA] text-[#333333]" style="font-family: 'Noto Sans KR'; font-weight: 500;">
+    
+    <div class="text-[#333333] mb-14 flex flex-col justify-center items-center space-y-3 " style="font-family: 'Noto Sans KR'; font-weight: 500;">
+        <div class="text-[35px]"><span class="text-[#C53528]">CONTACT</span> US</div>
+        <div class="text-[17px]">찾아오시는 길을 자세히 알려드립니다.</div>
+    </div>
+
+
+    <div class="w-full max-w-[1440px]">
+        <div id="map" class="w-full h-[550px] max-[500px]:h-[400px] "></div>
+    </div>
+    <div class="w-full max-w-[1440px] flex p-8 space-x-12">
+        <div><span class="mr-4 font-bold">주소</span><span class=" font-light">서울시 관악구 신림로 58길 13 S빌딩 302호</span></div>
+        <div><span class="mr-4 font-bold">전화</span><span class=" font-light">02-886-1305</span></div>
+        <div><span class="mr-4 font-bold">H.P</span><span class=" font-light">010-3139-5269</span></div>
+    </div>
+</div>
+
+<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=55022bcd2525beeddd280db1f43f0dbd"></script>
+<script src="./js/kakaoMap.js"></script>
+<!-- 지도 end -->
