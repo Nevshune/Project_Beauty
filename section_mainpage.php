dff6f1fe2e49c15c5f39c661aef8e844b27aa1fa
NamJiwan <129821189+NamJiwan@users.noreply.github.com>
2024-06-25 23:55:50 +0900

    메인케러셀 수정

--- section_mainpage.php
+++ section_mainpage.php
@@ -4,8 +4,8 @@
 
 
         <swiper-slide class="h-full w-full flex flex-col justify-center items-center text-3xl relative bg-[url('./src/img/Top-BG/BG001.jpg')] px-4 bg-cover  bg-center space-y-24">
-            <div class="w-full max-w-[800px] bg-white/40 backdrop-blur-md border-white/50 shadow-sm rounded-3xl">
-                <img src="./src/logo/JS_Logo-mainfull.svg" alt="" class="w-full py-12 px-20 max-[500px]:px-5 shadow-md">
+            <div class="w-full max-w-[800px] bg-white/30 backdrop-blur-sm border-white/50 shadow-sm rounded-3xl">
+                <img src="./src/logo/JS_Logo-mainfull.svg" alt="" class="w-full py-12 px-20 max-[700px]:px-5 max-[700px]:py-8 shadow-md">
             </div>
             <div class="w-full max-w-[1440px] text-xl text-white flex flex-col items-end px-[5%] max-md:hidden">
                 <p>피부만을 위한 전문 대표원장의 지도로</p>
@@ -24,9 +24,9 @@
 
         <swiper-slide class="h-full w-full flex flex-col justify-center items-center text-3xl relative bg-[url('./src/img/Top-BG/Top_bg.jpg')] px-4 bg-cover bg-[center] max-[580px]:bg-[center_left_-48px]">
             <div class=" absolute top-0 left-0 w-full bg-black h-[128px] flex items-center justify-center py-2">
-                <img src="./src/logo/JS_Logo-text_white.svg" alt="" class="h-full">
+                <img src="./src/logo/JS_Logo-text_white.svg" alt="" class="h-full max-[500px]:h-[60%]">
             </div>
-            <div class=" absolute bottom-0 left-0 w-full h- bg-black h-[128px] flex items-center justify-center py-4 text-white text-5xl max-[700px]:text-4xl font-extrabold">
+            <div class=" absolute bottom-0 left-0 w-full h- bg-black h-[128px] flex items-center justify-center py-4 text-white text-5xl max-[700px]:text-4xl  max-[500px]:text-2xl font-extrabold">
                 국제뷰티 EXPO 기능대회
             </div>
         </swiper-slide>
