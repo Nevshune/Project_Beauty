584e8ed35ad5712244f45f70e79c46901985ef5b
NamJiwan <129821189+NamJiwan@users.noreply.github.com>
2024-06-28 15:35:13 +0900

    모집과정 캐러셀수정

--- quickConsult.php
+++ quickConsult.php
@@ -8,7 +8,7 @@
             <h1 class="text-3xl text-nowrap">상담전화</h1>
             <p class="text-[#5879E2] font-bold text-nowrap">010-3139-5269</p>
         </div>
-        <div class="flex justify-between items-center max-[700px]:flex-col  max-[1120px]:justify-between w-full gap-3">
+        <div class="flex justify-between items-center max-[700px]:flex-col  max-[1120px]:justify-between w-full gap-3 max-[700px]:gap-2">
             <div class="flex gap-2  max-[1120px]:w-[90%] max-[700px]:w-full">
                 <div class="flex items-center gap-2 max-[700px]:w-[25%]">
                     <!-- <div>이름 : </div> -->
@@ -39,12 +39,12 @@
                     </select>
                 </div>
             </div>
-            <div class=" flex max-[1430px]:flex-col max-[700px]:flex-row max-[400px]:flex-col justify-center items-center   gap-2">
+            <div class=" flex max-[1430px]:flex-col max-[700px]:flex-row max-[400px]:flex-col justify-center items-center   gap-2 max-[400px]:gap-0">
                 <div class="flex items-center text-nowrap">
                     <input type="checkbox" id="chk">
                     <label for="chk" class="ps-1">개인정보 수집에 동의합니다.</label>
                 </div>
-                <button class="p-2  bg-gradient-to-r bg-[#7F00AC] rounded-full px-6 py-3 font-bold text-2xl text-white" type="button" onclick="sendPushRequest()">빠른상담</button> <!-- type="button" 추가 -->
+                <button class="p-2  bg-gradient-to-r bg-[#7F00AC] rounded-full px-6 py-3 font-bold text-2xl max-[700px]:text-base max-[700px]:py-1 text-white " type="button" onclick="sendPushRequest()">빠른상담</button> <!-- type="button" 추가 -->
             </div>
         </div>
         <a href="#" class="max-[700px]:hidden duration-75 flex-col items-center  "><i class="fa-solid fa-chevron-up ps-[5px] text-3xl leading-none text-[#777777]"></i>
@@ -52,7 +52,7 @@
         </a>
 
     </div>
-    <a href="#" class="min-[700px]:hidden max-[400px]:hidden absolute right-8 bottom-40 max-[400px]:bottom-48 w-[45px] h-[45px] duration-75 flex-col items-center 
+    <a href="#" class="min-[700px]:hidden max-[400px]:hidden absolute right-8 bottom-[125px]  w-[45px] h-[45px] duration-75 flex-col items-center 
     justify-stretch rounded-full bg-gradient-to-r from-[#8273F6] to-[#1651E8] text-center leading-none"><i class=" fa-solid fa-chevron-up text-xs pt-2 leading-none text-white"></i>
         <p class="text-sm leading-none text-white">TOP</p>
     </a>
--- section_advertisement.php
+++ section_advertisement.php
@@ -8,11 +8,11 @@ $result = $conn->query($sql);
     <div class="font-extrabold text-7xl mx-auto pb-24 text-[#000848] text-center max-[700px]:text-5xl ">모집중인 과정</div>
     <div class="w-full max-w-[1440px] m-auto relative pb-28 px-8 max-[500px]:px-8 flex-col justify-center items-center">
         <swiper-container id="test" class="mySwiper w-full h-full  " pagination="false" navigation="false" loop="true" autoplay-delay="2000" slides-per-view="1" space-between="30" breakpoints='{
-        "500": {
+        "550": {
           "slidesPerView": 2,
           "spaceBetween": 10
         },
-        "700": {
+        "820": {
           "slidesPerView": 3,
           "spaceBetween": 20
         },
@@ -23,9 +23,9 @@ $result = $conn->query($sql);
       }'>
             <?php while ($row = $result->fetch_assoc()) : ?>
                 <swiper-slide class="flex justify-center items-center">
-                    <div class="flex-col gap-8 max-[1150px]:flex-col justify-center items-center">
-                        <div class="w-full flex justify-center items-center rounded-md overflow-hidden">
-                            <img class="object-cover h-full" src="./Admin/uploads/class/<?php echo $row['classThumbnail']; ?>" alt="">
+                    <div class="w-full flex-col gap-8 max-[1150px]:flex-col justify-center items-center">
+                        <div class="w-full aspect-w-16 aspect-h-9 flex justify-center items-center rounded-md overflow-hidden">
+                            <img class="object-cover w-full" src="./Admin/uploads/class/<?php echo $row['classThumbnail']; ?>" alt="">
                         </div>
                         <div class="flex-col">
                             <div class="font-bold py-2 text-2xl"><?php echo $row['className']; ?></div>
@@ -50,3 +50,8 @@ $result = $conn->query($sql);
 
 
 </section>
+<style>
+    .aspect-w-16 {
+        aspect-ratio: 16 / 9;
+    }
+</style>
